593187b243710a7220ee0061dcb80e48bf645353
TinyProbe <137adv167@gmail.com>
2023-07-28 12:42:23 +0900

    update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 06:57:19 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/28 12:32:38 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/28 12:38:00 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 void add(int &a) { a += 10; }
 void attach(std::string &a) { a += "Hello World!"; }
-template <class T> void print_all(const T &a) {
+template <class T> void print(const T &a) {
 	static size_t i;
 	std::cout << "arr[" << (i++)%MAX_ARR << "]: " << a << ", ";
 }
@@ -25,14 +25,14 @@ template <class T> void print_all(const T &a) {
 int main() {
 	{
 		int arr[MAX_ARR] = {};
-		::iter(arr, MAX_ARR, print_all<int>); std::cout << '\n';
+		::iter(arr, MAX_ARR, print<int>); std::cout << '\n';
 		::iter(arr, MAX_ARR, add);
-		::iter(arr, MAX_ARR, print_all<int>); std::cout << '\n';
+		::iter(arr, MAX_ARR, print<int>); std::cout << '\n';
 	}
 	{
 		std::string arr[MAX_ARR] = {};
-		::iter(arr, MAX_ARR, print_all<std::string>); std::cout << '\n';
+		::iter(arr, MAX_ARR, print<std::string>); std::cout << '\n';
 		::iter(arr, MAX_ARR, attach);
-		::iter(arr, MAX_ARR, print_all<std::string>); std::cout << '\n';
+		::iter(arr, MAX_ARR, print<std::string>); std::cout << '\n';
 	}
 }
