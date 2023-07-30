0ec951bbfdaa0af16e5669bb0fd409f749eaaf0c
TinyProbe <137adv167@gmail.com>
2023-07-30 15:43:40 +0900

    update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 06:57:19 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/28 12:38:00 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/30 15:43:16 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 void add(int &a) { a += 10; }
 void attach(std::string &a) { a += "Hello World!"; }
 template <class T> void print(const T &a) {
+	static const size_t MAX = MAX_ARR;
 	static size_t i;
-	std::cout << "arr[" << (i++)%MAX_ARR << "]: " << a << ", ";
+	std::cout << "arr[" << i++ << "]: " << a << ", ";
+	if (i >= MAX) { i = 0; }
 }
 
 int main() {
